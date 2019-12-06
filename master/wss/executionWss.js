fd864ed7cfd4b4ce5c6c327a5b2984f7f6edf24d
Yosyp Mykhailiv <yosypmykhailiv@gmail.com>
2019-12-06 17:41:25 +0000

    Added handling of broken connections

--- master/wss/executionWss.js
+++ master/wss/executionWss.js
@@ -3,8 +3,14 @@ const axios = require('axios')
 const redis = require('../redis')
 
 // WebSocket for execution
-const executionWss = new WebSocket.Server({ noServer: true });
+const executionWss = new WebSocket.Server({ noServer: true })
 executionWss.on('connection', async (ws) => {
+
+  // set 'isAlive' property and callback for handling 'pong' event
+  ws.isAlive = true
+  ws.on('pong', () => {
+    ws.isAlive = true
+  })
 
   // ignore incoming messages
   ws.on('message', (message) => {})
@@ -14,15 +20,30 @@ executionWss.on('connection', async (ws) => {
 
   // make a request to execute the code
   axios.post(`http://runner/execute/${ws.payload.executionId}`)
-    .then(function (response) {
+    .then((response) => {
       ws.send(response.data.output)
+    })
+    .catch((error) => {
+      console.log(error) //@TODO implement error logging
+    })
+    .finally(() => {
       ws.close()
       redis.hDelAsync('execRequests', ws.payload.executionId)
     })
-    .catch(function (error) {
-      console.log(error) //@TODO implement error logging
-    })
 
-});
+})
 
-module.exports = executionWss;
+// Every 30 seconds check every WS connection
+setInterval(() => {
+  executionWss.clients.forEach((ws) => {
+    if (ws.isAlive === false) {
+      ws.terminate()
+      return
+    }
+
+    ws.isAlive = false
+    ws.ping(null, false, true)
+  })
+}, 30000)
+
+module.exports = executionWss
