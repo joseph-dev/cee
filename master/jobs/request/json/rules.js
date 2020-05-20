a1c997fc3c508c49dd48cc21a7b757e374f6ccbd
Yosyp Mykhailiv <yosypmykhailiv@gmail.com>
2020-05-20 20:10:28 +0200

    Added default locale to the runner and remove redundant parameters(maxProcesses, userId, activityId, lang)

--- master/jobs/request/json/rules.js
+++ master/jobs/request/json/rules.js
@@ -10,12 +10,8 @@ module.exports = {
     maxTime: {type: Number, required: true, range: `${eParams.time.min}-${eParams.time.max}`},
     maxFileSize: {type: Number, required: true},
     maxMemory: {type: Number, required: true, range: `${eParams.memory.min}-${eParams.memory.max}`},
-    maxProcesses: {type: Number, required: true},
-    userId: {type: String, required: true},
-    activityId: {type: String, required: true},
     execute: {type: String, required: true},
     interactive: {type: Number, required: true},
-    lang: {type: String, required: true},
     files: {
       type: Array,
       schema: {
