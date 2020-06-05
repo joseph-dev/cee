f4e6588d658a372bdb7314c6a36cb9c124924173
Yosyp Mykhailiv <yosypmykhailiv@gmail.com>
2020-06-05 11:47:45 +0200

    Updated the performance test to write logs

--- test/performance.js
+++ test/performance.js
@@ -1,23 +1,42 @@
 require('dotenv').config()
-const prepareExecutionRequestBody = require('./helpers/prepareExecutionRequestBody')
 const libxml = require("libxmljs")
+const {transports, createLogger, format} = require('winston')
 const WebSocket = require('ws')
+const prepareExecutionRequestBody = require('./helpers/prepareExecutionRequestBody')
+
+// init tools
 const axios = require('axios').create({
   baseURL: process.env.APP_URL
 })
+const logger = createLogger({
+  format: format.combine(
+    format.timestamp(),
+    format.json()
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: `logs/performance-${Date.now()}.log` })
+  ]
+})
 
+// prepare environment
 const contentType = process.env.API_TYPE === 'xml' ? 'application/xml' : 'application/json'
 const executionUrl = process.env.APP_TYPE === 'jail' ? '/' : `/${process.env.RUNNER_NAME}`
 
+// prepare request body
 const executionRequestBody = prepareExecutionRequestBody(process.env.API_TYPE, process.env.RUNNER_NAME, process.env.PAYLOAD_SCRIPT)
 
+// get execution start time
 const hrstart = process.hrtime()
 let numberOfFinishedExecutions = 0
 
+// start making requests
+logger.info("Starting execution")
 for (let i = 1; i <= process.env.NUMBER_OF_REQUESTS; i++) {
 
   axios.post(executionUrl, executionRequestBody, {headers: {'Content-Type': contentType}}).then(async (response) => {
 
+    // get execution ticket
     let executionTicket
     if (response.headers['content-type'].includes('text/xml')) {
       const xmlDoc = libxml.parseXmlString(response.data)
@@ -26,30 +45,33 @@ for (let i = 1; i <= process.env.NUMBER_OF_REQUESTS; i++) {
       executionTicket = response.data.executionTicket
     }
 
+    // create a websocket connection to get result
     const ws = new WebSocket(`${process.env.APP_SOCKET_URL}/${executionTicket}/execute`, {
       perMessageDeflate: false
     })
 
     ws.on('message', (data) => {
-      console.log(data)
+      logger.info(data)
     })
 
     ws.on('close', () => {
 
       numberOfFinishedExecutions++
-      // const ihrend = process.hrtime(hrstart)
-      // console.info(`${numberOfFinishedExecutions}) Execution time: ${ihrend[0]}s ${Math.floor(ihrend[1] / 1000000)}ms`, )
 
+      // calculate total execution time
       if (numberOfFinishedExecutions === parseInt(process.env.NUMBER_OF_REQUESTS)) {
         const hrend = process.hrtime(hrstart)
-        console.info(`Requests executed: ${numberOfFinishedExecutions}; Execution time: ${hrend[0]}s ${Math.floor(hrend[1] / 1000000)}ms;`)
+        logger.info({
+          executedRequest: numberOfFinishedExecutions,
+          executionTime: `${hrend[0]}s ${Math.floor(hrend[1] / 1000000)}ms`
+        })
       }
 
     })
 
-  }).catch(e => {
+  }).catch((e) => {
 
-    console.log(e)
+    logger.error(e)
 
   })
 
